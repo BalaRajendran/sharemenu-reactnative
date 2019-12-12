691aeb53b555a3eb7d4e3f80d59f09d41795dbb8
Balaji Rajendran <karurbalamathi@gmail.com>
2019-12-12 21:21:37 +0530

    menu

---
+++ App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+
+import RNFileShareIntent from 'react-native-file-share-intent';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fileUrl: null,
+    };
+  }
+
+  componentDidMount() {
+    if (RNFileShareIntent) {
+      RNFileShareIntent.getFilepath((url) => {
+        this.setState({ fileUrl: url });
+      })
+    }
+
+  }
+  render() {
+    var uri = this.state.fileUrl;
+    return (
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>Shared Url: {uri}</Text>
+      </View>
+    );
+  }
+}
